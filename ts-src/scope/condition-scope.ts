209f05c6a45daf38d1ed0e3e62f09e253e35324a
Franz Zemen <franzzemen@gmail.com>
2022-08-15 08:14:03 -0400

    ncu

--- ts-src/scope/condition-scope.ts
+++ ts-src/scope/condition-scope.ts
@@ -1,4 +1,5 @@
 import {ExecutionContextI} from '@franzzemen/app-utility';
+import {Scope} from '@franzzemen/re-common';
 import {ExpressionScope, ExpressionStackParser} from '@franzzemen/re-expression';
 import {ComparatorFactory} from '../comparator/comparator-factory';
 import {DataTypeComparatorFactory} from '../comparator/data-type-comparator/data-type-comparator-factory';
@@ -9,8 +10,8 @@ export class ConditionScope extends ExpressionScope {
   public static DataTypeComparatorFactory = 'DataTypeComparatorFactory';
   public static ComparatorFactory = 'ComparatorFactory';
 
-  constructor(options?: ConditionOptions, ec?:ExecutionContextI) {
-    super();
+  constructor(options?: ConditionOptions, parentScope?: Scope, ec?:ExecutionContextI) {
+    super(options, parentScope, ec);
     this.set(ConditionScope.DataTypeComparatorFactory, new DataTypeComparatorFactory());
     this.set(ConditionScope.ComparatorFactory, new ComparatorFactory());
     const parser = this.get(ConditionScope.ExpressionStackParser) as ExpressionStackParser;
