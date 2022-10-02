282f05903746b53477b5b891dd6834fa90243651
Franz Zemen <franzzemen@gmail.com>
2022-10-01 22:20:16 -0400

    ncu

--- ts-src/parser/condition-expression-parser.ts
+++ ts-src/parser/condition-expression-parser.ts
@@ -1,4 +1,4 @@
-import {ExecutionContextI, Hints, LoggerAdapter} from '@franzzemen/app-utility';
+import {ExecutionContextI, Hints} from '@franzzemen/app-utility';
 import {ParserMessages, PsMsgType} from '@franzzemen/re-common';
 import {StandardDataType} from '@franzzemen/re-data-type';
 import {
@@ -7,7 +7,7 @@ import {
   ExpressionReference,
   ExpressionScope,
   ExpressionStackParser,
-  ExpressionType, ExPsStdMsg
+  ExpressionType
 } from '@franzzemen/re-expression';
 import {ComparatorParser} from '../comparator/parser/comparator-parser.js';
 import {ConditionExpressionReference} from '../expression/condition-expression.js';
@@ -18,17 +18,18 @@ export class ConditionExpressionParser extends ExpressionParser {
   constructor() {
     super(ExpressionType.Condition);
   }
-  parse(remaining: string, scope:ExpressionScope, hints: Hints, ec?: ExecutionContextI): [string, ConditionExpressionReference, ParserMessages] {
+
+  parse(remaining: string, scope: ExpressionScope, hints: Hints, ec?: ExecutionContextI): [string, ConditionExpressionReference, ParserMessages] {
     // const log = new LoggerAdapter(ec, 'rules-engine', 'condition-expression-parser', `${ConditionExpressionParser.name}.parse`);
     const expressionStackParser: ExpressionStackParser = scope.get(ExpressionScope.ExpressionStackParser);
     const comparatorParser = new ComparatorParser(); // TODO add to Scope
     const near = remaining;
     let type: string = hints.get(ExpressionHintKey.Type) as string;
-    if(type && type !== ExpressionType.Condition) {
+    if (type && type !== ExpressionType.Condition) {
       return [remaining, undefined, undefined];
     }
     let dataTypeRef = hints.get(ExpressionHintKey.DataType) as string;
-    if(dataTypeRef) {
+    if (dataTypeRef) {
       if (dataTypeRef !== StandardDataType.Boolean) {
         if (type) {
           return [remaining, undefined, [{
@@ -46,13 +47,13 @@ export class ConditionExpressionParser extends ExpressionParser {
     // Check to see if the ?[]  standard format was used
     let standardFormat = false;
     let result = /^\?\[([^]*)$/.exec(remaining);
-    if(result) {
+    if (result) {
       standardFormat = true;
       remaining = result[1];
       type = ExpressionType.Condition;
     } else {
       // If the standard format was not used, a type hint was necessary
-      if(!type) {
+      if (!type) {
         return [remaining, undefined, undefined];
       }
     }
@@ -63,50 +64,11 @@ export class ConditionExpressionParser extends ExpressionParser {
     let rhsRef: ExpressionReference;
     let messages: ParserMessages;
     [remaining, lhsRef, comparatorRef, rhsRef, messages] = ConditionParser.parseComparativeCondition(remaining, scope, ec);
-    if(!lhsRef || !comparatorRef || !rhsRef) {
+    if (!lhsRef || !comparatorRef || !rhsRef) {
       return [near, undefined, messages];
     }
-
-    /*
-      // Keep a copy of remaining in case this is not a condition expression
-      //let candidateRemaining = remaining;
-    [remaining, lhsRef] = expressionStackParser.parse(remaining, scope, {}, ec);
-    if(!lhsRef) {
-      return [near, undefined, undefined];
-    }
-    // We're going to try and consume a comparator, but it may not be the right one if we have indeterminate or unknown data types.  Remember candidateRemaining.
-    let candidateRemaining = remaining;
-    [remaining, comparatorRef] = comparatorParser.parse(remaining, lhsRef.dataTypeRef, scope, ec);
-    if(!comparatorRef) {
-      return [near, undefined, undefined];
-    }
-    if(lhsRef.dataTypeRef === StandardDataType.Unknown || lhsRef.dataTypeRef === StandardDataType.Indeterminate) {
-      [remaining, rhsRef] = expressionStackParser.parse(remaining, scope, undefined, ec);
-    } else {
-      [remaining, rhsRef] = expressionStackParser.parse(remaining, scope, {inferredDataType: lhsRef.dataTypeRef}, ec);
-    }
-    // Reconcile indeterminate and possibly reprocess comparator
-    if(lhsRef.dataTypeRef === StandardDataType.Indeterminate && rhsRef.dataTypeRef !== StandardDataType.Indeterminate) {
-      // Reprocess comparator, discarding string result. It may or may not be the same comparatorRef
-      [candidateRemaining,comparatorRef] = comparatorParser.parse(candidateRemaining, rhsRef.dataTypeRef, scope, ec);
-      if(!comparatorRef) {
-        return [near, undefined, [{message: `No comparator to parse near ${candidateRemaining}`, contextObject: {lhsRef, rhsRef}, type: PsMsgType.Info}]];
-      }
-      lhsRef.dataTypeRef = rhsRef.dataTypeRef;
-    } else if (rhsRef.dataTypeRef === StandardDataType.Indeterminate && lhsRef.dataTypeRef !== StandardDataType.Indeterminate) {
-      rhsRef.dataTypeRef = lhsRef.dataTypeRef;
-    } else { // Both indeterminate
-      if(scope.get(ExpressionScope.AllowUnknownDataType) === true) {
-        lhsRef.dataTypeRef = StandardDataType.Unknown;
-        rhsRef.dataTypeRef = StandardDataType.Unknown;
-      } else {
-        return [near, undefined, [{message: `${ExPsStdMsg.ImproperUsageOfUnknown} and both lhs and rhs are indeterminate near ${near}`, contextObject: {lhsRef, rhsRef}, type: PsMsgType.Error}]];
-      }
-    }
-
-     */
     // Check for closure
-    if(standardFormat) {
+    if (standardFormat) {
       let result2 = /^]([^]*)$/.exec(remaining);
       if (result2) {
         remaining = result2[1].trim();
@@ -114,35 +76,6 @@ export class ConditionExpressionParser extends ExpressionParser {
         return [near, undefined, undefined];
       }
     }
-    return [remaining,  {type, dataTypeRef, lhsRef, rhsRef, comparatorRef}, undefined];
+    return [remaining, {type, dataTypeRef, lhsRef, rhsRef, comparatorRef}, undefined];
   }
-
-/*
-  parse(remaining: string, scope: Map<string, any>, ec?: ExecutionContextI) : [string, ConditionReference] {
-    const log = new LoggerAdapter(ec, 'rules-engine', 'condition-parser', `${ConditionParser.name}.parse`);
-    const expressionStackParser: ExpressionStackParser = scope.get(ScopeKey.ExpressionStackParser);
-    let conditionRef: Partial<ConditionReference> = {};
-    let near = remaining;
-
-    [remaining, conditionRef.lhsRef] = expressionStackParser.parse(remaining, scope, undefined, ec);
-    const comparatorParser = new ComparatorParser(); // TODO add to Scope
-
-    [remaining, conditionRef.comparatorRef] = comparatorParser.parse(remaining, conditionRef.lhsRef.dataTypeRef, scope, ec);
-
-    // Note that for the RHS, we don't pass in the hints since they would have been consumed by the LHS.  We also pass in the suggested data type
-    // based on the LHS
-    [remaining, conditionRef.rhsRef] = expressionStackParser.parse(remaining, scope, conditionRef.lhsRef.dataTypeRef, ec);
-
-
-    if(conditionRef.rhsRef.dataTypeRef !== conditionRef.lhsRef.dataTypeRef) {
-      log.warn(conditionRef, `Inconsistent data types near '${near}'`);
-      const err = new Error(`Inconsistent data types lhs:${conditionRef.lhsRef.dataTypeRef} vs rhs:${conditionRef.rhsRef.dataTypeRef} near '${near}'`);
-      log.error(err);
-      throw err;
-    }
-    return [remaining, conditionRef as ConditionReference];
-  }
-
- */
-
 }
--- ts-src/parser/condition-parser.ts
+++ ts-src/parser/condition-parser.ts
@@ -1,4 +1,4 @@
-import {ExecutionContextI, LoggerAdapter} from '@franzzemen/app-utility';
+import {ExecutionContextI} from '@franzzemen/app-utility';
 import {FragmentParser, ParserMessages, PsMsgType} from '@franzzemen/re-common';
 import {StandardDataType} from '@franzzemen/re-data-type';
 import {ExpressionReference, ExpressionScope, ExpressionStackParser, ExPsStdMsg} from '@franzzemen/re-expression';
@@ -13,8 +13,7 @@ export class ConditionParser extends FragmentParser<ConditionReference>{
   }
 
   parse(remaining: string, scope:ConditionScope, ec?: ExecutionContextI) : [string, ConditionReference, ParserMessages] {
-    const log = new LoggerAdapter(ec, 're-condition', 'condition-parser', `${ConditionParser.name}.parse`);
-    const expressionStackParser: ExpressionStackParser = scope.get(ExpressionScope.ExpressionStackParser);
+    // const log = new LoggerAdapter(ec, 're-condition', 'condition-parser', `${ConditionParser.name}.parse`);
     let conditionRef: Partial<ConditionReference> = {};
     let messages: ParserMessages;
     [remaining, conditionRef.lhsRef, conditionRef.comparatorRef, conditionRef.rhsRef, messages] = ConditionParser.parseComparativeCondition(remaining, scope, ec);
@@ -67,62 +66,5 @@ export class ConditionParser extends FragmentParser<ConditionReference>{
       }
     }
     return [remaining, lhsRef, comparatorRef, rhsRef, undefined];
-
-    /*
-    const expressionStackParser: ExpressionStackParser = scope.get(ExpressionScope.ExpressionStackParser);
-
-    // Attempt to parse RHS comparator LHS
-    // The data type may be inferrable in RHS and not LHS, so allow the data type to be undefined.
-    let lhsRef: ExpressionReference;
-    let comparatorRef: string;
-    let rhsRef: ExpressionReference;
-    // Keep a copy of remaining in case this is not a condition expression
-    let candidateRemaining = remaining;
-    [candidateRemaining, lhsRef] = expressionStackParser.parse(candidateRemaining, scope, {}, ec);
-    if(!lhsRef) {
-      return [remaining, undefined, undefined, undefined, undefined];
-    }
-    // We're going to try and consume a comparator, but it may not be the right one.  Remember candidateRemaining.
-    let candidateRemaining2 = candidateRemaining;
-    [candidateRemaining2, comparatorRef] = comparatorParser.parse(candidateRemaining2, lhsRef.dataTypeRef, scope, true, ec);
-    if(!comparatorRef) {
-      const err = new Error(`Expected comparator near '${candidateRemaining2}'`);
-      log.error(err);
-      throw err;
-    }
-    [candidateRemaining2, rhsRef] = expressionStackParser.parse(candidateRemaining2, scope, {inferredDataType: lhsRef.dataTypeRef}, ec);
-    // Check for a data type
-    // If the lhsRef data type exists, then it is the right comparator, because we provided it in the call.
-    // If it does not exist, and rhsRef data type exists, then we have to reprocess the comparator to make sure we didn't parse something we should not have
-    if(lhsRef.dataTypeRef) {
-      // We have the right comparator
-      // Check that RHS has the same data type
-      if (rhsRef.dataTypeRef !== lhsRef.dataTypeRef) {
-        log.warn(`Inconsistent data types near '${remaining}'`);
-        const err = new Error(`Inconsistent data types lhs:${lhsRef.dataTypeRef} vs rhs:${rhsRef.dataTypeRef} near '${remaining}'`);
-        log.error(err);
-        throw err;
-      }
-      return [candidateRemaining2, lhsRef, comparatorRef, rhsRef, undefined];
-    } else {
-      if(rhsRef.dataTypeRef) {
-        // Now we need to reprocess the comparator to make sure we have the right one.
-        let [, reprocessed] = comparatorParser.parse(candidateRemaining, rhsRef.dataTypeRef, scope, false, ec);
-        if(!reprocessed) {
-          const err = new Error(`No comparator for rhs data type ${rhsRef.dataTypeRef}`);
-          log.error(err);
-          throw err;
-        }
-        if(reprocessed !== comparatorRef) {
-          return [remaining, undefined, undefined, undefined, undefined];
-        }
-        lhsRef.dataTypeRef = rhsRef.dataTypeRef;
-        return [candidateRemaining2, lhsRef, comparatorRef, rhsRef, undefined];
-      } else {
-        const err = new Error(`No data types on lhs or rhs`);
-        log.error(err);
-        throw err;
-      }
-    }*/
   }
 }
