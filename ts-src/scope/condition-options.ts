d601312a211e329a408b20035cc2063c3109cd66
Franz Zemen <franzzemen@gmail.com>
2022-10-09 07:54:01 -0400

    mergeConditionOptions

--- ts-src/scope/condition-options.ts
+++ ts-src/scope/condition-options.ts
@@ -1,5 +1,9 @@
-import {ExpressionOptions} from '@franzzemen/re-expression';
+import {_mergeExpressionOptions, ExpressionOptions} from '@franzzemen/re-expression';
 
 export interface ConditionOptions extends ExpressionOptions {
 
+}
+
+export function _mergeConditionOptions(source: ConditionOptions, target: ConditionOptions, mergeInto = true): ConditionOptions {
+  return _mergeExpressionOptions(source, target, mergeInto);
 }
